e1c5291629e120b26263a7107c99b189b1db0a13
Alefig12 <62145703+Alefig12@users.noreply.github.com>
2022-08-11 22:05:29 -0500

    adding assets

--- day3/12-counterWithCanny.py
+++ day3/12-counterWithCanny.py
@@ -13,7 +13,7 @@ cnts, _ = cv2.findContours(canny,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE) # Wi
 #Using same process as before to draw and count objects found
 cv2.drawContours(img, cnts, -1, (0,0,255), 2, cv2.LINE_AA)
 text = 'Found: {} objects'.format(len(cnts))
-cv2.putText(img, text, (0,25), 0, 1, (0,0,0), 1,cv2.LINE_AA)
+cv2.putText(img, text, (0,25), 0, 1, (0,0,0), 2,cv2.LINE_AA)
 
 
 
