b0f0d090e79c8b73c39c1a677c9b66ab9f476d51
Alefig12 <62145703+Alefig12@users.noreply.github.com>
2022-08-11 22:38:24 -0500

    minor corrections

--- day1/5-colorDetection.py
+++ day1/5-colorDetection.py
@@ -5,9 +5,10 @@ cap = cv2.VideoCapture(0)
 # Useful stackoverflow answers: https://stackoverflow.com/questions/10948589/choosing-the-correct-upper-and-lower-hsv-boundaries-for-color-detection-withcv?lq=1
 # Color ranges based on: https://i.stack.imgur.com/gyuw4.png
 
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+out = cv2.VideoWriter('output_assets/output_colorDetection.mp4',fourcc, 30.0, (640,480))
 
-
-lower_blue = np.array([90,30, 20], dtype=np.uint8)
+lower_blue = np.array([90,50, 20], dtype=np.uint8)
 upper_blue = np.array([140,255, 255], dtype=np.uint8)
 
 while True:
@@ -25,10 +26,10 @@ while True:
     cv2.imshow('video', color_mask)
 
 
-
+    out.write(color_mask)
     if (cv2.waitKey(1) & 0xFF == ord('q')):
         break
 
 cap.release()
-
+out.release()
 cv2.destroyAllWindows()
